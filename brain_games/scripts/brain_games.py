7cc0594127dcdef108d581c2efe5b29b8960dcb9
vnk005 <vnk@bk.ru>
2024-03-05 10:39:50 +0300

    file modification

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,9 +3,11 @@
 
 from  brain_games.cli import welcome_user
 
+player_name = None
+
 
 def main():
-    print("Welcome to the Brain Games!")
+    player_name = welcome_user()
 
 
 if __name__ == '__main__':
