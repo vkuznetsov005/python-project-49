5afb8ef18ca3096f7d129b38387070f7f9035717
Vladimir Kuznetsov <vnk@bk.ru>
2025-02-11 18:04:49 +0300

    modifed files

---
+++ brain_games/logic.py
@@ -0,0 +1,9 @@
+from brain_games.logic import brain_even
+
+
+def main():
+	even_game()
+
+
+if __name__ == '__main__':
+	main()
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,12 +1,4 @@
 #!/usr/bin/env python3
 
 
-from brain_games.even import even_game
 
-
-def main():
-    even_game()
-
-
-if __name__ == '__main__':
-    main()
